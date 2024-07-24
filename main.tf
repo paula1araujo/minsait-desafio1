bcb3d91e0189ded39a31e536e57f061d2a7ec36b
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 10:10:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
@@ -112,3 +111,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
 output "public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
